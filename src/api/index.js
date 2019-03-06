d0c56cd01cc34b05afe96fb764a6f6523789e71c
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-03-07 00:16:37 +0900

    로그인

--- src/api/index.js
+++ src/api/index.js
@@ -2,23 +2,30 @@ import axios from "axios"
 import router from "../router"
 
 const DOMAIN = 'http://localhost:3000'
-const UNAUTHORIZED = 401
-const onUnauthorized = () => {
-    router.push('/login')
-}
+// const UNAUTHORIZED = 401
+// const onUnauthorized = () => {
+//     router.push('/login')
+// }
 
 const request = (method, url, data) => {
+    const {token} = localStorage
+    if (token) setAuthInHeader(token)
     return axios({
         method,
         url: DOMAIN + url,
         data
     })
     .then(result => result.data)
-    .catch(error => {
+    .catch(result => {
+        //console.log(result.response)
         const { status } = result.response
-        if (status === UNAUTHORIZED) return onUnauthorized()
-        throw Error(error)
+        //if (status === UNAUTHORIZED) return onUnauthorized()
+        throw result.response
     })
+}
+
+export const setAuthInHeader = token => {
+    axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 }
 
 export const board = {
@@ -26,3 +33,10 @@ export const board = {
         return request('get', '/boards')
     }
 } 
+
+
+export const auth = {
+    login(email, password) {
+        return request('post', '/login', {email, password})
+    }
+}
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,20 +6,9 @@
             <div v-if="loading">Loading...</div>
             <div v-else>
                 <div v-for="b in boards" :key="b.id">
-                    {{b}}
+                    <pre>{{b}}</pre>
                 </div>
             </div>
-            <ul>
-                <li>
-                    <router-link to="/b/1">Board 1</router-link>
-                </li>
-                <li>
-                    <router-link to="/b/2">Board 2</router-link>
-                </li>
-                <li>
-                    <router-link to="/b/3">Board 3</router-link>
-                </li>
-            </ul>
         </div>
     </div>
 </template>
