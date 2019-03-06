8315ac20127dcb5c19d446c2197c256301c970b7
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-03-06 21:43:35 +0900

    중첩 라우트 및 데이터 가져오는 방법

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         Board
-        <div>bid : {{bid}}</div>
+        <div v-if="loading">loading board...</div>
+        <div v-else>
+            <div>bid : {{bid}}</div>
+            <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+            <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+            <router-link :to="`/b/${bid}/c/3`">Card 3</router-link>
+        </div>
+        <hr />
+        <router-view></router-view>
     </div>
 </template>
 
@@ -9,12 +17,22 @@
 export default {
     data() {
         return {
-            bid: 0
+            bid: 0,
+            loading: false
         }
     },
     created() {
         //console.log(this.$route.params.bid)
-        this.bid = this.$route.params.bid
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            this.loading = true
+            setTimeout(() => {
+                this.bid = this.$route.params.bid
+                this.loading = false
+            }, 500);
+        }
     }
 }
 </script>
