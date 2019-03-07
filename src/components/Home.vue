3eda55e4186994a71ec3a40704d664708b51312c
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-03-07 22:17:50 +0900

    보드 목록 조회 화면

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-        Home
-        <div>
-            Board List:
-            <div v-if="loading">Loading...</div>
-            <div v-else>
-                <div v-for="b in boards" :key="b.id">
-                    <pre>{{b}}</pre>
-                </div>
+        <div class="home-title">Personal Boards</div>
+        <div class="board-list">
+            <div class="board-item" v-for="b in boards" :key="b.id" :data-bgcolor="b.bgColor" ref="boardItem">
+                <router-link :to="`/b/${b.id}`">
+                    <div class="board-item-title">{{b.title}}</div>
+                </router-link>
+            </div>
+            <div class="board-item">
+                <a class="new-board-btn" href="" @click.prevent="addBoard">
+                    Create new board...
+                </a>
             </div>
         </div>
     </div>
@@ -20,18 +23,24 @@ export default {
     data() {
         return {
             loading: false,
-            boards: []
+            boards: [],
+            error: ''
         }
     },
     created() {
         this.fetchData()
+    },
+    updated() {
+        this.$refs.boardItem.forEach(el => {
+            el.style.backgroundColor = el.dataset.bgcolor
+        })
     },
     methods: {
         fetchData() {
             this.loading = true
             board.fetch()
                 .then(data => {
-                    this.boards = data
+                    this.boards = data.list
                 })
                 .finally(() => {
                     this.loading = false
@@ -42,5 +51,46 @@ export default {
 </script>
 
 <style>
-
+.home-title {
+    padding: 10px;
+    font-size: 18px;
+    font-weight: bold;
+}
+.board-list {
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.board-item {
+    width: 23%;
+    height: 100px;
+    margin: 0 2% 20px 0;
+    border-radius: 3px;
+    }
+.board-item a {
+    text-decoration: none;
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+    background-color: rgba(0,0,0, .1);
+    color: #666;
+}
+.board-item-title {
+    color: rgb(255, 255, 255);
+    font-size: 18px;
+    font-weight: 700;
+    padding: 10px;
+}
+.board-item a.new-board-btn {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    height: 100px;
+    width: inherit;
+    color: #888;
+    font-weight: 700;
+    }
 </style>
