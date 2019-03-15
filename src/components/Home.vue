35e3575c02f101f8bfd5a9e71dfdcd918b1906f4
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-03-15 20:28:58 +0900

    vuex 보드 목록 조회

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,27 +13,27 @@
                 </a>
             </div>
         </div>
-        <AddBoard v-if="isAddBoard" @close="isAddBoard=false" @submit="onAddBoard" />
+        <AddBoard v-if="isAddBoard" @close="isAddBoard=false" />
     </div>
 </template>
 
 <script>
 import { board } from "../api"
 import AddBoard from "./AddBoard.vue"
-import { mapState, mapMutations } from 'vuex'
+import { mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
     components: { AddBoard },
     data() {
         return {
             loading: false,
-            boards: [],
             error: ''
         }
     },
     computed: {
         ...mapState([
-            'isAddBoard' 
+            'isAddBoard',
+            'boards'
         ])
     },
     created() {
@@ -48,18 +48,14 @@ export default {
         ...mapMutations([
             'SET_IS_ADD_BOARD'
         ]),
+        ...mapActions([
+            'FETCH_BOARDS'
+        ]),
         fetchData() {
             this.loading = true
-            board.fetch()
-                .then(data => {
-                    this.boards = data.list
-                })
-                .finally(() => {
+            this.FETCH_BOARDS().finally(() => {
                     this.loading = false
                 })
-        },
-        onAddBoard(title) {
-            this.fetchData()
         }
     }
 }
