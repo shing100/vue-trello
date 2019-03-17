44d98deb7d901fc468064bc834fd36ff557d46a5
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-03-17 16:35:24 +0900

    보드 조회화면 개발

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -4,6 +4,7 @@
         <div v-if="loading">loading board...</div>
         <div v-else>
             <div>bid : {{bid}}</div>
+            <pre>{{board}}</pre>
             <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
             <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
             <router-link :to="`/b/${bid}/c/3`">Card 3</router-link>
@@ -14,6 +15,8 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
     data() {
         return {
@@ -21,17 +24,23 @@ export default {
             loading: false
         }
     },
+    computed: {
+        ...mapState([
+            board: 'board'
+        ])
+    },
     created() {
         //console.log(this.$route.params.bid)
         this.fetchData()
     },
     methods: {
+        ...mapActions([
+            'FETCH_BOARD'
+        ]),
         fetchData() {
             this.loading = true
-            setTimeout(() => {
-                this.bid = this.$route.params.bid
-                this.loading = false
-            }, 500);
+            this.FETCH_BOARD({id: this.$route.params.bid})
+                .then(() => this.loading = false)
         }
     }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,13 +1,7 @@
 import { setAuthInHeader } from '../api'
 
 const mutations = {
-    SET_IS_ADD_BOARD(state, toggle) {
-        state.isAddBoard = toggle
-    },
-    SET_BOARDS(state, boards) {
-        state.boards = boards
-    },
-    LOGIN(state, token) {
+     LOGIN(state, token) {
         if (!token) return
         state.token = token
         localStorage.setItem('token', token)
@@ -17,6 +11,15 @@ const mutations = {
         state.token = null
         delete localStorage.token
         setAuthInHeader(null)
+    },
+    SET_IS_ADD_BOARD(state, toggle) {
+        state.isAddBoard = toggle
+    },
+    SET_BOARDS(state, boards) {
+        state.boards = boards
+    },
+    SET_BOARD(state, board) {
+        state.board = board
     }
 }
 
